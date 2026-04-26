fce3a7f79b49fe200aa611594b3aad04f4dd1705
caioinsaxd <caiosantanaparize2015@gmail.com>
2026-04-26 17:52:31 -0300

    feat: registrando use cases

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./infrastructure/database/database.module";
 import { GamesController } from "./presentation/controllers/games.controller";
+import { PlaceBetUseCase } from "./application/use-cases/place-bet.use-case";
+import { CashoutUseCase } from "./application/use-cases/cashout.use-case";
+import { GameService } from "./application/services/game.service";
 
 @Module({
   imports: [DatabaseModule],
   controllers: [GamesController],
+  providers: [PlaceBetUseCase, CashoutUseCase, GameService],
 })
 export class AppModule {}
